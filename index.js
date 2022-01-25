d5af7f13606c2f55912697204f2bcd2d10a7bb4d
Your Name <you@example.com>
2022-01-25 10:52:50 +0200

    music player

---
+++ index.js
@@ -0,0 +1,17 @@
+// music
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+function togglePlay() {
+  if (isPlaying) {
+    myAudio.pause()
+  } else {
+    myAudio.play();
+  }
+};
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
