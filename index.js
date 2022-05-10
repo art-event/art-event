f8a112f898bfe744210835b24c51287856fc7f13
Your Name <you@example.com>
2022-05-10 10:38:55 +0300

    magic

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-/* // music
-var myAudio = document.getElementById("myAudio");
-var isPlaying = false;
-
-function togglePlay() {
-  if (isPlaying) {
-    myAudio.pause()
-  } else {
-    myAudio.play();
-  }
-};
-myAudio.onplaying = function() {
-  isPlaying = true;
-};
-myAudio.onpause = function() {
-  isPlaying = false;
-};
- */
 
 
 
